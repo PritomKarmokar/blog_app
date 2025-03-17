f83eccd0dedf45f9124904fb2864ba4cd4d10fb5
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-03-17 23:01:55 +0600

    feat: adding serializer

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+"""
+class Post:
+    id int
+    title str(50)
+    content text
+    created_at datetime
+"""
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
