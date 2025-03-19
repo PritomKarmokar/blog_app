36ea048aaccc71b5dda2bb17c9edc3b5117207a9
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-03-19 13:26:26 +0600

    refractor: migrating from func based views to class based

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import homepage, list_posts, post_details, update_post, delete_post
+from .views import homepage, list_posts, post_details, update_post, delete_post, PostListCreateView, PostRetrieveUpdateDeleteView
 
 urlpatterns = [
     path("homepage/", homepage, name="posts_home"),
-    path("", list_posts, name="list_posts"),
-    path("<int:post_id>/", post_details, name="post_details"),
-    path("update/<int:post_id>/", update_post, name="update_post"),
-    path("delete/<int:post_id>/", delete_post, name="delete_post"),
+    path("", PostListCreateView.as_view(), name="list_or_create_post"),
+    path("<int:post_id>/", PostRetrieveUpdateDeleteView.as_view(), name="post_details"),
+    # path("update/<int:post_id>/", update_post, name="update_post"),
+    # path("delete/<int:post_id>/", delete_post, name="delete_post"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,8 +3,9 @@ import logging
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
 
+from django.http import Http404
 from django.shortcuts import get_object_or_404
 
 from .models import Post
@@ -91,3 +92,94 @@ def delete_post(request: Request, post_id: int):
     post = get_object_or_404(Post, pk=post_id)
     post.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+class PostListCreateView(APIView):
+    """
+        A View for creating and listing posts
+    """
+    serializer_class = PostSerializer
+    def get(self, request: Request, *args, **kwargs):
+        posts = Post.objects.all()
+        serializer = self.serializer_class(instance=posts, many=True)
+        response = {
+            "message": "Available Posts",
+            "posts": serializer.data,
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
+    
+    def post(self, request: Request, *args, **kwargs):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            response = {
+                "message": "New Post created successfully",
+                "data": serializer.data
+            }
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class PostRetrieveUpdateDeleteView(APIView):
+    serializer_class = PostSerializer
+
+    def get(self, request: Request, post_id: int):
+        post = get_object_or_404(Post, pk=post_id)
+        
+        serializer = self.serializer_class(instance=post)
+
+        response = {
+            "message": "Aviable post",
+            "data": serializer.data,
+        }
+
+        return Response(data=response, status=status.HTTP_200_OK)
+        
+
+    def put(self, request: Request, post_id: int):
+        data = request.data
+        post = get_object_or_404(Post, pk=post_id)
+
+        serializer = self.serializer_class(instance=post, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            response = {
+                "message": "Post Updated",
+                "data": serializer.data,
+            }
+
+            return Response(data=response, status=status.HTTP_200_OK)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # def delete(self, request: Request, post_id: int):
+    #     post = get_object_or_404(Post, pk=post_id)
+
+    #     post.delete()
+
+    #     response = {
+    #         "message": f"Post with Id {post_id} deleted successfully",
+    #     }
+
+    #     return Response(data=response, status=status.HTTP_204_NO_CONTENT)
+        
+    """
+        Adding ways to handle self error response
+    """
+    def delete(self, request, post_id: int):
+        try:
+            post = get_object_or_404(Post, pk=post_id)  # Raises Http404 if not found
+            post.delete()
+            response = {"message": f"Post with ID {post_id} deleted successfully"}
+            return Response(data=response, status=status.HTTP_204_NO_CONTENT)
+        
+        except Http404:
+            response = {"message": f"Post with ID {post_id} not found"}
+            return Response(data=response, status=status.HTTP_404_NOT_FOUND)
+        
+        
